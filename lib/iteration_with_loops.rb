19d1f1ea7ab794999357f6bc58f0a50f79bd760f
Elena Dinca <elenadinca25@gmail.com>
2020-04-04 17:16:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -22,30 +22,16 @@ mixed_data = [
 ]
 =end
 
-outer_results = []
+array = 0
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  all_string_element = ""
   while element_index < src[row_index].count do
-
-    # Array at row_index
-    # Element of the inner array at element_index
-    # If the length of the current element is greater than the length of longest_string_element
-    # Set longest_string_element to the current element
-    if src[row_index][element_index].length < all_string_element.length
-      all_string_element = src[row_index][element_index]
-    end
+    array += src[row_index][element_index]
     element_index += 1
-  end
 
-  # We have to check every element in each inner array, so after the loop finishes
-  # The current value of longest_string_element is pushed into outer_results
-  outer_results << all_string_element
+  end
   row_index += 1
 end
-
-outer_results.join("")
-
-
+array
 end
