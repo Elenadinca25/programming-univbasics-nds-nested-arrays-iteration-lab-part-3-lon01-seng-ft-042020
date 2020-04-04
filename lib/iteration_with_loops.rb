ab87b9a0df9089918d9e2da815e3f2d1daaea0fc
Elena Dinca <elenadinca25@gmail.com>
2020-04-04 16:06:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  array[row_index] = "#{src[0][0]} #{src[0][2]} #{src[1][1]} #{src[[1][2]} #{src[2][0]} #{src[2][3]}"
+  array[row_index] = "#{src[row_index]}"
   row_index += 1
 end
   array.join("")
