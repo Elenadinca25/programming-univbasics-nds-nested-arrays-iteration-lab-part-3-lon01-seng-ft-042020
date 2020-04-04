ecb83c9f08e58e590ce1cfbac645597ff83b9e5c
Elena Dinca <elenadinca25@gmail.com>
2020-04-04 18:56:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ row_index = 0
 new_array = " "
 while row_index < src.count do
   element_index = 0
-  while element_index < src[index].count do
+  while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
       new_array += src[row_index][element_index] + ' '
 end
